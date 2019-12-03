010da1b797432023dec5756c3d793092e8b26a78
yqzhang <yqzhang@winnermedical.com>
2019-12-03 20:32:23 +0800

    测试

---
+++ src/test/java/com/yunboot/common/pojo/common/Car.java
@@ -0,0 +1,20 @@
+package com.yunboot.common.pojo.common;
+
+import lombok.Data;
+
+/**
+ * [简要描述]:<br/>
+ * [详细描述]:<br/>
+ *
+ * @author yqzhang
+ * @version 1.0, 2019/6/5 15:21
+ * @since JDK 1.8
+ */
+@Data
+public class Car
+{
+    private int discount;
+
+    private Person person;
+
+}
---
+++ src/test/java/com/yunboot/common/pojo/common/Person.java
@@ -0,0 +1,17 @@
+package com.yunboot.common.pojo.common;
+
+import lombok.Data;
+
+/**
+ * [简要描述]:<br/>
+ * [详细描述]:<br/>
+ *
+ * @author yqzhang
+ * @version 1.0, 2019/6/5 15:22
+ * @since JDK 1.8
+ */
+@Data
+public class Person
+{
+    int age;
+}

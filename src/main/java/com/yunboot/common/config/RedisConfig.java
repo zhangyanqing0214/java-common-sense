bb1f0d0033844e6b949f54d4348857a9b5f54fba
yqzhang <yqzhang@winnermedical.com>
2019-12-10 11:36:24 +0800

    test

--- src/main/java/com/yunboot/common/config/RedisConfig.java
+++ src/main/java/com/yunboot/common/config/RedisConfig.java
@@ -1,29 +1,19 @@
 package com.yunboot.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig
 {
-
-    @Value("${repository.redis.master.name:NONE}")
-    private String masterName = "master";
-    @Value("${repository.redis.master.port:26379}")
-    private int port = 26389;
-    @Value("${repository.redis.master.host:NONE}")
-    private String host = "192.168.206.200";
-    @Value("${repository.redis.sentinel.list:NONE}")
-    private String sentinelListString = "192.168.206.200:26389";
-    @Value("${repository.redis.sentinel.password}")
-    private String password = "123456";
-    @Value("${repository.redis.master.databases}")
-    private int databases = 0;
-    @Value("${repository.redis.host.name:NONE}")
-    private String hostName;
-    @Value("${repository.redis.host.port:6379}")
-    private String hostPort;
+    private String masterName;
+    private int port;
+    private String host;
+    private String sentinelListString;
+    private String password = "jl!@12";
+    private int databases = 9;
+    private String hostName = "192.168.206.204";
+    private int hostPort = 6789;
 
     @Bean
     public YunBootJedisPoolConfig buildYunBootJedisPoolConfig()
@@ -33,26 +23,23 @@ public class RedisConfig
 
     /**
      * [简要描述]:哨兵配置
-     * 
-     * @author yqzhang
+     *
      * @return
      */
     @Bean
     public YunBootRedisSentinelConfiguration bulidYunBootRedisSentinelConfiguration()
     {
-        if (masterName.equals("NONE"))
+        if (masterName == null || masterName.equals("NONE"))
         {
             return null;
         }
-        YunBootRedisSentinelConfiguration redisSentinelConfiguration = new YunBootRedisSentinelConfiguration(masterName,
-                host, port);
+        YunBootRedisSentinelConfiguration redisSentinelConfiguration = new YunBootRedisSentinelConfiguration(masterName, host, port);
         redisSentinelConfiguration.setSentinelListString(sentinelListString);
         return redisSentinelConfiguration;
     }
 
     @Bean
-    public YunBootRedisConnectionFactory bulidYunBootRedisConnectionFactory(
-            YunBootRedisSentinelConfiguration sentinelConfiguration, YunBootJedisPoolConfig jedisPoolConfig)
+    public YunBootRedisConnectionFactory bulidYunBootRedisConnectionFactory(YunBootRedisSentinelConfiguration sentinelConfiguration, YunBootJedisPoolConfig jedisPoolConfig)
     {
         YunBootRedisConnectionFactory factory = null;
         if (sentinelConfiguration != null)
@@ -63,7 +50,7 @@ public class RedisConfig
         else
         {
             // redis工厂配置-单节点模式
-            factory = new YunBootRedisConnectionFactory(jedisPoolConfig, hostName, port);
+            factory = new YunBootRedisConnectionFactory(jedisPoolConfig, hostName, hostPort);
         }
         factory.setPassword(password);
         factory.setDatabase(databases);
